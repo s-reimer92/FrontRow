4236b26cfa5628b352f58a57bf20c94dae91f341
s-reimer92 <shane.reimer92@gmail.com>
2018-11-21 00:54:04 -0800

    First versions of setlist and lastfm search functions

---
+++ lastfm.js
@@ -0,0 +1,31 @@
+const request = require('request');
+
+
+//Takes a search term as a parameter and returns a list of 10 results
+var getArtists = (artist, callback) => {
+	request({
+		url: "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist=" + encodeURIComponent(artist) + "&api_key=e006c67d7936e45b2cfa8fed71da22a6&format=json",
+		json: true,
+	}, (error, response, body) => {
+		if (body.results.artistmastches == undefined) {
+			callback("No Results")
+		} else {
+			var searchresults = []
+			for (var i=0; i<10; i++){
+				searchresults.push(JSON.stringify(body.results.artistmatches.artist[i].name, undefined, 2))	
+			}
+			callback(searchresults)
+		}
+	});
+};
+
+
+module.exports = {
+	getArtists
+}
+
+/* TEST FUNCTION
+getArtists('black', (results) => {
+	console.log(results);
+});
+*/
