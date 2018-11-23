259ce93ad5954bbe54990a1e4f2f249c5c1d89d8
s-reimer92 <shane.reimer92@gmail.com>
2018-11-23 13:40:00 -0800

    Added a bitching logo in the header

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const lastfm = require('./lastfm.js')
 
 var app = express();
 
@@ -9,15 +10,27 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+//Holds value for search bar
+var currentSearch
 
-app.get('/', (request, response) => {
+
+app.get('/', (request, response) => {	
 	response.render('home.hbs', {
-		title: 'Front Row',
-		image: 'logo.png'
+		title: 'Front Row'
 	})
 })
 
+app.get('/login', (request, response) => {
+	response.render('login.hbs', {
+		title: 'Log In',
+	})
+})
 
+app.get('/signup', (request, response) => {
+	response.render('signup.hbs', {
+		title: 'Sign Up',
+	})
+})
 
 const port = process.env.PORT || 8080;
 
